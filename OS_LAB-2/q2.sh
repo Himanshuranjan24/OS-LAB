bcaf10c8f9e6002f48b79c4177b448051db06f3f
Himanshuranjan24 <himanshusingh24022002@gmail.com>
2023-02-02 19:44:37 +0530

    bash

---
+++ OS_LAB-2/q2.sh
@@ -0,0 +1,10 @@
+<<question
+Take the input in sec and display the time in hrs,min,sec format
+question
+#!/bin/bash
+echo "Enter time in seconds:"
+read seconds
+hours=$((seconds / 3600))
+minutes=$((seconds % 3600 / 60))
+remaining_seconds=$((seconds % 60))
+echo "Time in hh:mm:ss format: $hours:$minutes:$remaining_seconds"
